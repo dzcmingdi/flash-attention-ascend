fda0def0aace55bcf9b42ba3de1569728bae30c3
dzcmingdi <2379415638@qq.com>
2023-04-20 18:09:31 +0800

    update

--- tbe/impl/flash_attention.py
+++ tbe/impl/flash_attention.py
@@ -5,30 +5,64 @@ import numpy
 # from tbe.common.utils import para_check
 
 ai_core_num = tbe_platform.get_soc_spec("CORE_NUM")
+L1_BUFFER_SIZE = tbe_platform.get_soc_spec("L1_SIZE")
+
+def block_matmul(q_shape, k_shape, q, k, out, tik_instance):
+    M = q_shape[1]
+
+    N = k_shape[1]
+
+    K1 = q_shape[0]
+    K0 = 16
+
+    q_cbuf = tik_instance.Tensor(dtype='float16', shape=q_shape, name='q_cbuf', scope=tik.scope_cbuf)
+    k_cbuf = tik_instance.Tensor(dtype='float16', shape=k_shape, name='k_cbuf', scope=tik.scope_cbuf)
+
+    tik_instance.data_move(q_cbuf, q, 0, 1, (q_shape[0] * q_shape[1] * q_shape[2] * 2) // 32, 0, 0)
+    tik_instance.data_move(k_cbuf, k, 0, 1, (k_shape[0] * k_shape[1] * k_shape[2] * 2) // 32, 0, 0)
+
+    # input_q_cbuf = input_q_cbuf.reshape([2, 16, 16])
+    # input_k_cbuf = input_k_cbuf.reshape([2, 16, 16])
+
+    dst_ubuf = tik_instance.Tensor(dtype='float32', shape=[N // 16, M // 16 * 16, 16], name='dst_ubuf',
+                                   scope=tik.scope_cbuf_out)
+    tik_instance.matmul(dst_ubuf, q_cbuf, k_cbuf, M, K0 * K1, N)
+
+    output = tik_instance.Tensor(dtype='float16', shape=dst_ubuf.shape, name='output', scope=tik.scope_gm)
+
+    tik_instance.fixpipe(output, dst_ubuf, N // 16, (M * 64) // 32, 0, 0,
+                         {"quantize_params": {"mode": "fp322fp16", "mode_param": None}})
+
+    return output
 
 
-def flash_attention(input_q, input_k, kernel_name="FlashAttention"):
-    tbe_platform.set_current_compile_soc_info("Ascend310")
+def flash_attention(q_type, k_type, kernel_name="FlashAttention"):
     tik_instance = tik.Tik(disable_debug=False)
-    input_q = tik_instance.Tensor(dtype='float32', shape=[24], name="input_q", scope=tik.scope_gm)
-    output = tik_instance.Tensor(dtype='float32', shape=[24], name='output', scope=tik.scope_gm)
-    with tik_instance.for_range(0, 2, block_num=ai_core_num) as i:
-        shape = 16 - (i * 16 + 16) // 24 * 8
-        input_q_ubuf = tik_instance.Tensor(dtype='float32', shape=[shape], name="input_q_ubuf",
-                                           scope=tik.scope_ubuf)
-        tik_instance.data_move(input_q_ubuf, input_q[i * 16], 0, 1, shape // 8, 0, 0)
-        tik_instance.vec_abs(shape, input_q_ubuf, input_q_ubuf, 1, 0, 0)
-        tik_instance.data_move(output[i * 16], input_q_ubuf, 0, 1, shape // 8, 0, 0)
 
-    tik_instance.BuildCCE(kernel_name='FlashAttention', inputs=[input_q], outputs=[output])
+    q_shape = q_type['shape']
+    k_shape = k_type['shape']
+    q = tik_instance.Tensor(dtype='float16', shape=q_type['shape'], name="q", scope=tik.scope_gm)
+    k = tik_instance.Tensor(dtype='float16', shape=k_type['shape'], name='k', scope=tik.scope_gm)
+
+    tik_instance.BuildCCE(kernel_name="FlashAttention", inputs=[q, k], outputs=[])
     return tik_instance
 
 
 if __name__ == '__main__':
-    tik_instance = flash_attention(None, None)
-    data = -numpy.ones((24,), dtype=numpy.float32)
-    feed_dict = {'input_q': data}
+    tbe_platform.set_current_compile_soc_info("Ascend310")
+    print(L1_BUFFER_SIZE)
+
+
+    # input_q = numpy.random.random(size=(16, 32)).astype(numpy.float16)
+    # input_k = numpy.random.random(size=(32, 16)).astype(numpy.float16)
+    #
+    # input_q = input_q.reshape((16, 2, 16)).transpose((1, 0, 2))
+    # input_k = input_k.reshape((2, 16, 16)).transpose((0, 2, 1))
+    # feed_dict = {'q': input_q, 'k': input_k}
+    #
+    # tik_instance = flash_attention({'shape': input_q.shape}, {'shape': input_k.shape})
+    #
     # 启动功能调试
-    o, = tik_instance.tikdb.start_debug(feed_dict=feed_dict, interactive=True)
-    print(o)
+    # o, = tik_instance.tikdb.start_debug(feed_dict=feed_dict, interactive=True)
+    # print(o)
     # 打印输出数据
